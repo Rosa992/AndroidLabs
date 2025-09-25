eac9b48de3d907de58e1067285e3d9910a0eb63c
Rosa <rosahamilton07@gmail.com>
2025-09-25 11:00:36 -0400

    commit constraint, grid and linear file

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -14,11 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        CheckBox checkBox = findViewById(R.id.myCheckBox);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                String message = "The checkbox is now " + (isChecked ? "on" : "off");
+
+                Snackbar.make(cb, message, Snackbar.LENGTH_LONG)
+                        .setAction("Undo", v -> cb.setChecked(!isChecked))
+                        .show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+            EditText editText = findViewById(R.id.editTextInput);
+            Button button = findViewById(R.id.buttonPressMe);
+            TextView textView = findViewById(R.id.textViewOutput);
+
+            button.setOnClickListener(v -> {
+                String inputText = editText.getText().toString();
+                textView.setText(inputText);
+
         });
+
     }
 }
